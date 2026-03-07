687ad246011e83f6077de0365fd9b7088cdaa1f8
João Santos <2019.j.biel@gmail.com>
2026-03-07 15:43:10 -0300

    Coloquei confirmação de senha e tentativas
    Limite de tentativas e troquei a mensagem final

--- #======= inicio =====.py
+++ #======= inicio =====.py
@@ -1,9 +1,19 @@
 #======= inicio =====
 print('CADASTRE-SE aqui!')
-
+tentativas = 0
 #========Pegando o usuário e a senha=======
-usuario = input('Crie um nome de usuário: ').strip()
-senha = input('Crie uma senha: ').strip()
+while True:
+    usuario = input('Crie um nome de usuário: ').strip()
+    senha = input('Crie uma senha: ').strip()
+    confirmaçao = input('Confirme a senha: ').strip()
+    if confirmaçao == senha:
+        break
+    elif senha != confirmaçao:
+        print('-' * 30)
+        print('Senhas diferente! Tente novamente!')
+
+
+
 
 #mensagem
 print('Parabéns! Agora você tem uma conta!')
@@ -15,11 +25,19 @@ print('Login abaixo')
 while True:
     usuario2 = input('Nome de usuário: ').strip()
     senha2 = input('Digite a senha: ').strip()
+    tentativas += 1
     if usuario2 == usuario and senha2 == senha:
         break
+    elif tentativas == 3:
+        mensagem_bloqueada = "conta bloaqueada"
+        break
     else:
-        print('Senha ou Usuário incorreto!')
-        print('Tente novamente!')
-
+        print('-' * 30)
+        print('Senha ou Usuário incorreto! Tente novamente!')
+        print(f'Você fez {tentativas} tentativas de 3')
 #=========Fim=======
-print('Login realizado com sucesso!')
+if tentativas < 4:
+    print('Conta bloqueada!')
+    print('Motivo: Atingiu o limite de tentativas!')
+else:
+    print('Login realizada com sucesso!')

6011a25243417710a1c9f45340114d3f74150dcb
João Santos <2019.j.biel@gmail.com>
2026-03-07 18:56:23 -0300

    Juntei meu login com um jogo de advinhação

--- #======= inicio =====.py
+++ #======= inicio =====.py
@@ -11,17 +11,14 @@ while True:
     elif senha != confirmaçao:
         print('-' * 30)
         print('Senhas diferente! Tente novamente!')
-
-
-
-
-#mensagem
+        
+#=======mensagem=======
 print('Parabéns! Agora você tem uma conta!')
 print('-' *  35)
 print('Login abaixo')
 
 
-#=======usando o while=======
+#=======Sistema de login=======
 while True:
     usuario2 = input('Nome de usuário: ').strip()
     senha2 = input('Digite a senha: ').strip()
@@ -36,8 +33,101 @@ while True:
         print('Senha ou Usuário incorreto! Tente novamente!')
         print(f'Você fez {tentativas} tentativas de 3')
 #=========Fim=======
-if tentativas < 4:
+if tentativas == 3:
     print('Conta bloqueada!')
     print('Motivo: Atingiu o limite de tentativas!')
 else:
-    print('Login realizada com sucesso!')
+    cert = print('Login realizada com sucesso!')
+
+#=======Fim do código=======
+
+#======= inicio do jogo de advinhação =======
+if cert == print('Login realizada com sucesso!'):
+    print(" ")
+    print('-' * 30)
+    print(" ")
+    print('Agora você tem acesso ao jogo de advinhação!')
+
+
+#=====Biblioteca=====
+import random
+
+
+
+
+#=========Nome=========
+
+print('🎮 Jogo de Adivinhação\U0001F3AF!')
+
+
+
+#======Pegando dificuldade=======
+
+print('1️⃣ Fácil 2️⃣ Médio 3️⃣ Difícil ')
+d = str(input('Qual dificuldade você deseja: ').strip())
+
+
+
+#=====Variavel para tentativas=====
+t = 0
+
+#=====Sobre as dificuldades=====
+
+if d == '1':
+    f = random.randint(1, 10)
+
+elif d == '2':
+    f = random.randint(1, 50)
+
+elif d == '3':
+    f = random.randint(1, 100)
+
+#Guardando a dificuldade
+
+if d == '1':
+    limite = '10'
+elif d == '2':
+    limite = '50'
+elif d == '3':
+    limite = '100'
+
+
+#=====Para funcionarr=====
+
+while True:
+    p = int(input("🕵️ Descubra o número secreto entre 1 e {}: ".format(limite)))
+    t += 1 #calculando dificuldade
+    if f == p:
+         print('🏆 Você acertou! Parabéns!!! 🎉')
+         break
+    
+    elif p > f:
+        print('⬇️ Tente um número menor!')
+
+    elif p < f:
+        print('⬆️ Tente um número maior!')
+
+    else:
+         print('Você errou! Tente novamente!')
+
+#=====Clasificação======
+
+if t == 1:
+    c = 'Excelente'
+
+elif t <= 5:
+    c = 'Ótimo'
+
+elif t <= 10:
+    c = 'Bom'
+
+else:
+    c = 'Ruim'
+
+
+#=====Fim=====
+
+print('🎯Você conseguiu em {} tentativas!'.format(t))
+print("⭐ Classificação: {}".format(c))
+
+    

34293c8e055083af3cfc13499b9fa1f5c06a9d0b
João Santos <2019.j.biel@gmail.com>
2026-03-08 10:47:38 -0300

    Coloquei menu e a opção de jogar novamente

--- Jogo com login sem repetição.py
+++ Jogo com login sem repetição.py
@@ -1,130 +1,120 @@
-#======= inicio =====
+import random
+
+# ======== CADASTRO ========
 print('CADASTRE-SE aqui!')
-tentativas = 0
-#======== Pegando o usuário e a senha =======
 while True:
     usuario = input('Crie um nome de usuário: ').strip()
     senha = input('Crie uma senha: ').strip()
-    confirmaçao = input('Confirme a senha: ').strip()
-    if confirmaçao == senha:
+    confirmacao = input('Confirme a senha: ').strip()
+
+    if confirmacao == senha:
         break
-    elif senha != confirmaçao:
+    else:
         print('-' * 30)
-        print('Senhas diferente! Tente novamente!')
-        
-#======= Mensagem de boas-vindas =======
+        print('Senhas diferentes! Tente novamente!')
+
 print('Parabéns! Agora você tem uma conta!')
-print('-' *  35)
+print('-' * 35)
 print('Login abaixo')
 
+# ======== LOGIN ========
+tentativas = 0
 
-#=======Sistema de login=======
 while True:
     usuario2 = input('Nome de usuário: ').strip()
     senha2 = input('Digite a senha: ').strip()
     tentativas += 1
+
     if usuario2 == usuario and senha2 == senha:
+        print('Login realizado com sucesso!')
         break
     elif tentativas == 3:
-        mensagem_bloqueada = "conta bloaqueada"
-        break
+        print('Conta bloqueada!')
+        print('Motivo: Atingiu o limite de tentativas!')
+        exit()
     else:
         print('-' * 30)
-        print('Senha ou Usuário incorreto! Tente novamente!')
-        print(f'Você fez {tentativas} tentativas de 3')
+        print('Senha ou usuário incorreto! Tente novamente!')
+        print(f'Você fez {tentativas} tentativa(s) de 3')
 
-#========= Fim do programa de login =======
-
-if tentativas == 3:
-    print('Conta bloqueada!')
-    print('Motivo: Atingiu o limite de tentativas!')
-else:
-    cert = print('Login realizada com sucesso!')
-
-#======= Fim do código de Login =======
-
-#======= inicio do jogo de advinhação =======
-if cert == print('Login realizada com sucesso!'):
-    print(" ")
-    print('-' * 30)
-    print(" ")
-    print('Agora você tem acesso ao jogo de advinhação!')
-
-
-#===== Biblioteca de random=====
-import random
-
-
-#========= Nome do jogo =========
-
-print('🎮 Jogo de Adivinhação\U0001F3AF!')
-
-
-#===== Variavel para tentativas =====
-t = 0
-
-#====== Pegando dificuldade e colocando pra funcionar =======
-
+# ======== MENU PRINCIPAL ========
 while True:
-    print('1️⃣ Fácil 2️⃣ Médio 3️⃣ Difícil ')
-    d = str(input('Qual dificuldade você deseja: ').strip())
-    if d == '1':
-        f = random.randint(1, 10)
+    print('\n' + '=' * 30)
+    print('MENU PRINCIPAL')
+    print('1 - Jogar')
+    print('2 - Sair')
+    print('=' * 30)
+
+    menu = input('Escolha uma opção: ').strip()
+
+    if menu == '1':
+        # ======== REPETIR O JOGO ========
+        while True:
+            print('\n🎮 Jogo de Adivinhação 🎯')
+
+            t = 0
+
+            # ======== ESCOLHA DA DIFICULDADE ========
+            while True:
+                print('1️⃣ Fácil   2️⃣ Médio   3️⃣ Difícil')
+                d = input('Qual dificuldade você deseja: ').strip()
+
+                if d == '1':
+                    f = random.randint(1, 10)
+                    limite = 10
+                    break
+                elif d == '2':
+                    f = random.randint(1, 50)
+                    limite = 50
+                    break
+                elif d == '3':
+                    f = random.randint(1, 100)
+                    limite = 100
+                    break
+                else:
+                    print('Escolha somente essas opções!')
+
+            # ======== FUNCIONAMENTO DO JOGO ========
+            while True:
+                try:
+                    p = int(input(f'🕵️ Descubra o número secreto entre 1 e {limite}: '))
+                    t += 1
+
+                    if p == f:
+                        print('🏆 Você acertou! Parabéns!!! 🎉')
+                        break
+                    elif p > f:
+                        print('⬇️ Tente um número menor!')
+                    else:
+                        print('⬆️ Tente um número maior!')
+                except ValueError:
+                    print('Digite apenas números!')
+
+            # ======== CLASSIFICAÇÃO ========
+            if t == 1:
+                c = 'Excelente'
+            elif t <= 5:
+                c = 'Ótimo'
+            elif t <= 10:
+                c = 'Bom'
+            else:
+                c = 'Ruim'
+
+            print(f'🎯 Você conseguiu em {t} tentativas!')
+            print(f'⭐ Classificação: {c}')
+
+            # ======== JOGAR NOVAMENTE ========
+            novamente = input('Quer jogar novamente? (s/n): ').strip().lower()
+
+            if novamente != 's':
+                break
+
+    elif menu == '2':
+        print('Programa encerrado!')
         break
-    elif d == '2':
-        f = random.randint(1, 50)
-        break
-    elif d == '3':
-        f = random.randint(1, 100)
-        break
+
     else:
-        print('Escolha somente essas opções!')
-#======= Guardando a dificuldade ========
+        print('Escolha uma opção válida!')
 
-if d == '1':
-    limite = '10'
-elif d == '2':
-    limite = '50'
-elif d == '3':
-    limite = '100'
-
-
-#===== Funcionamento do jogo =====
-
-while True:
-    p = int(input("🕵️ Descubra o número secreto entre 1 e {}: ".format(limite)))
-    t += 1 #calculando dificuldade
-    if f == p:
-         print('🏆 Você acertou! Parabéns!!! 🎉')
-         break
-    
-    elif p > f:
-        print('⬇️ Tente um número menor!')
-
-    elif p < f:
-        print('⬆️ Tente um número maior!')
-
-    else:
-         print('Você errou! Tente novamente!')
-
-#===== Guardando a classificação ======
-
-if t == 1:
-    c = 'Excelente'
-
-elif t <= 5:
-    c = 'Ótimo'
-
-elif t <= 10:
-    c = 'Bom'
-
-else:
-    c = 'Ruim'
-
-
-#===== Fim do jogo =====
-
-print('🎯Você conseguiu em {} tentativas!'.format(t))
-print("⭐ Classificação: {}".format(c))
 
     
